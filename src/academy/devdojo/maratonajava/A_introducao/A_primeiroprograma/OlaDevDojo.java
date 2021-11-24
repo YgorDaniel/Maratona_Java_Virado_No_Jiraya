23edc9c179260a51756a43a77e3caca0a87d99d3
ygordaniel <ygordaniel@DESKTOP-KR7OABJ>
2021-11-24 19:28:25 -0300

    primeiro programa

---
+++ src/academy/devdojo/maratonajava/A_introducao/A_primeiroprograma/OlaDevDojo.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.A_introducao.A_primeiroprograma;
+
+public class OlaDevDojo {
+	public static void main(String[]args) {
+		System.out.print("Ka-Me-Ha-Hah");
+	}
+
+}
